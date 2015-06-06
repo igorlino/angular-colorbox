8571578ea555f956abe50922ea85af6a1380089f
Igor Lino <i.lino@directica.pl>
2015-06-07 01:14:38 +0200

    Force element directive so that scope can be isolated. Remove javascript from demo.

--- js/colorbox.directive.js
+++ js/colorbox.directive.js
@@ -7,53 +7,107 @@
     colorboxDirective.$inject = ['$compile', '$rootScope', '$http', '$parse', '$timeout'];
     function colorboxDirective($compile, $rootScope, $http, $parse, $timeout) {
         var service = {
-            restrict: 'A',
+            restrict: 'E',
             scope: {
-                /*formObject: '=',
-                 formErrors: '=',
-                 title: '@',
-                 template: '@',
-                 okButtonText: '@',
-                 formSubmit: '&'*/
+                open: '=',
+                templateUrl: '&',
 
-                cbOnOpen: '&colorboxOnOpen',
-                cbOnLoad: '&colorboxOnLoad',
-                cbOnComplete: '&colorboxOnComplete',
-                cbOnCleanup: '&colorboxOnCleanup',
-                cbOnClosed: '&colorboxOnClosed'
+                onOpen: '&',
+                onLoad: '&',
+                onComplete: '&',
+                onCleanup: '&',
+                onClosed: '&'
 
             },
-            //compile: compile,
-            link: link
+            require: 'colorbox',
+            link: link,
+            controller: controller,
+            controllerAs: 'vm'
         };
         return service;
 
         ////////////////////////////
 
+        controller.$inject = ['$scope'];
+        function controller($scope) {
+            console.log($scope.open);
+        }
 
-        link.$inject = ['scope', 'element', 'attrs'];
-        function link(scope, element, attrs) {
-            element.click('bind', function () {
+        link.$inject = ['$scope', '$element', '$attributes'];
+        function link($scope, $element, $attributes, controller) {
+            var cb = null;
 
+            /*$scope.$watch('open', function (newValue, oldValue) {
+             console.log("watch $scope.open("+$scope.open+") " + oldValue + "->" + newValue);
+             if (oldValue !== newValue) {
+             updateOpen();
+             }
+             });*/
+
+            $scope.$on('$destroy', function () {
+                $element.remove();
+            });
+
+            updateOpen();
+
+            function updateOpen() {
+                /*if ($scope.open) {
+                 init();
+                 } else {
+                 if (cb) {
+                 $.colorbox.close();
+                 }
+                 }*/
+                init();
+            }
+
+            function init() {
+                if (cb) {
+                    return;
+                }
                 var options = {
-                    href: attrs.colorboxSrc,
-                    onOpen: scope.cbOnOpen(),
-                    onLoad: scope.cbOnLoad(),
-                    onComplete: function () {
-                        onComplete();
-                        if (scope.cbOnComplete()) {
-                            scope.cbOnComplete()();
+                    href: $attributes.src,
+                    boxFor: $attributes.boxFor,
+                    onOpen: function () {
+                        $scope.open = true;
+                        console.log("open")
+                        if ($scope.onOpen()) {
+                            $scope.onOpen()();
                         }
                     },
-                    onCleanup: scope.cbOnCleanup(),
-                    onClosed: scope.cbOnClosed()
+                    onLoad: function () {
+                        console.log("load")
+                        if ($scope.onLoad()) {
+                            $scope.onLoad()();
+                        }
+                    },
+                    onComplete: function () {
+                        console.log("complete")
+                        onComplete();
+                        if ($scope.onComplete()) {
+                            $scope.onComplete()();
+                        }
+                    },
+                    onCleanup: function () {
+                        console.log("cleanup")
+                        if ($scope.onCleanup()) {
+                            $scope.onCleanup()();
+                        }
+                    },
+                    onClosed: function () {
+                        console.log("closing")
+                        $scope.open = false;
+                        if ($scope.onClosed()) {
+                            $scope.onClosed()();
+                        }
+                    }
                 };
 
                 //generic way that sets all (non-function) parameters of colorbox.
-                if (attrs.colorboxOptions) {
-                    var cbOptionsFunc = $parse(attrs.colorboxOptions);
-                    var cbOptions = cbOptionsFunc(scope);
-                    angular.copy(cbOptions, options);
+                if ($attributes.options) {
+                    var cbOptionsFunc = $parse($attributes.options);
+                    var cbOptions = cbOptionsFunc($scope);
+                    angular.extend(options, cbOptions);
                 }
 
                 //clean undefined
@@ -65,32 +119,26 @@
                     }
                 }
 
-                $timeout(function () {
-                    if (options.href) {
-                        //opens the colorbox using an href.
-                        $.colorbox(options);
-                    } else if (attrs.colorboxSelector) {
-                        //opens the colorbox using all related elements by the selected.
-                        $(attrs.colorboxSelector).colorbox(options);
-                    } else {
-                        //opens the element of the directive.
-                        $(element).colorbox(options);
-                    }
+
+                if (options.boxFor) {
+                    //opens the element by id boxFor
+                    cb = $(options.boxFor).colorbox(options);
+                } else if (options.href) {
+                    //opens the colorbox using an href.
+                    cb = $.colorbox(options);
+                }
+
+            }
+
+            function onComplete() {
+                $rootScope.$apply(function () {
+                    var content = $('#cboxLoadedContent');
+                    $compile(content)($rootScope);
                 });
-
-            });
-
-            scope.$on('$destroy', function () {
-                element.remove();
-            });
+            }
         }
 
-        function onComplete() {
-            $rootScope.$apply(function () {
-                var content = $('#cboxLoadedContent');
-                $compile(content)($rootScope);
-            });
-        }
+
     }
 
     /*
